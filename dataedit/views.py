aa7d6a2132072bde0b951968a9fd4cc8d89458a8
MGlauer <martinglauer89@gmail.com>
2017-12-13 16:37:38 +0100

    Fix dump download

--- dataedit/views.py
+++ dataedit/views.py
@@ -408,6 +408,7 @@ def get_dependencies(schema, table, found=None):
 
 
 def create_dump(schema, table, fname):
+    os.environ['PGPASSWORD'] = sec.dbpasswd
     assert re.match(actions.pgsql_qualifier, table)
     assert re.match(actions.pgsql_qualifier, schema)
     for path in ['/dumps', '/dumps/{schema}'.format(schema=schema),
@@ -418,7 +419,7 @@ def create_dump(schema, table, fname):
          sec.dbuser, '-h', sec.dbhost, '-p',
          str(sec.dbport), '-d', sec.dbname, '-f',
          sec.MEDIA_ROOT + '/dumps/{schema}/{table}/'.format(schema=schema, table=table) + fname+'.dump'] + reduce(add, (['-n', s, '-t', s + '.' + t] for s,t in get_dependencies(schema,table)),[])
-    return call(L, shell=False)
+    yield call(L, shell=False)
 
 
 def send_dump(schema, table, fname):
